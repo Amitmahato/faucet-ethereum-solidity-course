d4998e9c073a27613af2cd038e6af4a68c0deb04
Amit Mahato <amit643.mahato@gmail.com>
2022-08-23 17:47:57 +0545

    remove any storage variables & add receive function in faucet contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,5 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-  string public name = "Amit";
+  
+  /**
+    - this is a special function
+    - it's called when you make a tx that doesn't specify function name to call
+    - It executes on calls to the contract with no data (calldata), e.g. calls made via send() or transfer().
+    
+    - External function are part of the contract interface 
+      which means they can be called via contracts and other txs
+   */
+
+  receive() external payable {
+    // React to receiving ether
+  }
 }
+
+// Block info
+// Nonce - a hash that when combined with the minHash proves that
+// the block has gone through proof of work(POW)
+// 8 bytes => 64 bits
