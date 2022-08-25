c1d8b0ee36a7dcbb45a88c3e9c2e9c194fd37e46
Amit Mahato <amit643.mahato@gmail.com>
2022-08-25 14:57:12 +0545

    add a pure method and info about pure & view methods

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -17,6 +17,21 @@ contract Faucet {
   }
 
   function addFunds() external payable{}
+
+  /**
+    modifiers: `pure` & `view` - no any gas fee incurs 
+      view - indicates the given function will not change the storage state in any way
+      pure - even more strict, it will not even allow reading the storage state
+   */
+   function justTesting() external pure returns(uint) {
+     return 2 + 2;
+   }
+
+  // Transactions: generates chanages in storage state & incurs gas fees
+
+
+  // In order to talk to the nodes on the network, we can make JSON-RPC http call, 
+  // which is actually being used internally by `truffle` & `web3`
 }
 
 // Block info
