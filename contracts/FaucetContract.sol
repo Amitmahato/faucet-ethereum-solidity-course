73e24561ff601134a2879cd9ffe3a0751b03e1a5
Amit Mahato <amit643.mahato@gmail.com>
2022-08-25 23:15:16 +0545

    add storage variable to store fund, number of funders, funders mapping their addresses, methods to get funds and get funder address at given address

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -6,7 +6,9 @@ contract Faucet {
   // `public` - can be accessible throw the getter method outside the smart contract
   // `private` - can be accessible only within the smart contract
   // `internal` - can be accessible within the smart contract and also the derived smart contracts
-  address[] public funders;
+  uint public numberOfFunders;
+  uint fund;
+  mapping(uint => address) public funders;
 
   /**
     - this is a special function
@@ -21,7 +23,22 @@ contract Faucet {
     // React to receiving ether
   }
 
-  function addFunds() external payable{}
+  function addFunds() external payable{
+    uint index = numberOfFunders++;
+    funders[index] = msg.sender;
+    if (msg.value > 0) {
+      fund += msg.value;
+    }
+  }
+
+  function getAvailableFund() public view returns(uint) {
+    return fund;
+  }
+
+  function getFunderAtIndex(uint index) public view returns ( address funder) {
+    funder = funders[index];
+  //  return funders[index];
+  }
 
   /**
     modifiers: `pure` & `view` - no any gas fee incurs 
@@ -43,3 +60,24 @@ contract Faucet {
 // Nonce - a hash that when combined with the minHash proves that
 // the block has gone through proof of work(POW)
 // 8 bytes => 64 bits
+
+/**
+    Commands to:
+     // get an instance of Faucet contract
+     const instance = await Faucet.deployed();
+
+     1. add funds 
+     => await instance.addFunds({ from: accounts[0], value: 1500000000000000000 }) // 1.5 ETH
+     => await instance.addFunds({ from: accounts[1], value: 1700000000000000000 }) // 1.7 ETH
+
+     2. get fund
+     => (await instance.getAvailableFund()).toString()
+
+     3. get number of funders
+     => (await instance.numberOfFunders()).toString();
+
+     4. get funder at a given index
+     => await instance.getFunderAtIndex(0);
+     => await instance.getFunderAtIndex(1);
+     => await instance.getFunderAtIndex(2);
+ */
