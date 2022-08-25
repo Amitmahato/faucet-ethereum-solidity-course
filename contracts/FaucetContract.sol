911ba9ea5c0b3fdbbf135cde84b270884f4d6cb3
Amit Mahato <amit643.mahato@gmail.com>
2022-08-26 00:12:04 +0545

    struct based funders & prevent duplication - struct based funder info and look up t able for funders to get funder at index or all the funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -7,8 +7,13 @@ contract Faucet {
   // `private` - can be accessible only within the smart contract
   // `internal` - can be accessible within the smart contract and also the derived smart contracts
   uint public numberOfFunders;
-  uint fund;
-  mapping(uint => address) public funders;
+  uint private fund;
+  struct FunderInfo {
+    uint numberOfDonations;
+    uint totalDonations;
+  }
+  mapping(address => FunderInfo) private funders;
+  mapping(uint => address) private lookUpFunders;
 
   /**
     - this is a special function
@@ -24,10 +29,24 @@ contract Faucet {
   }
 
   function addFunds() external payable{
-    uint index = numberOfFunders++;
-    funders[index] = msg.sender;
+    address funderAddress = msg.sender;
+
     if (msg.value > 0) {
       fund += msg.value;
+    }
+
+    if(funders[funderAddress].numberOfDonations == 0){
+      uint index = numberOfFunders++;
+      
+      funders[funderAddress] = FunderInfo({
+        numberOfDonations : 1,
+        totalDonations : msg.value
+      });
+
+      lookUpFunders[index] = funderAddress;
+    } else {
+      funders[funderAddress].numberOfDonations += 1;
+      funders[funderAddress].totalDonations += msg.value;
     }
   }
 
@@ -35,14 +54,14 @@ contract Faucet {
     return fund;
   }
 
-  function getFunderAtIndex(uint index) public view returns ( address funder) {
-    funder = funders[index];
+  function getFunderAtIndex(uint index) public view returns ( FunderInfo memory funder) {
+    funder = funders[lookUpFunders[index]];
   }
 
-  function getAllFunders() public view returns(address[] memory) {
-    address[] memory _funders = new address[](numberOfFunders);
+  function getAllFunders() public view returns(FunderInfo[] memory) {
+    FunderInfo[] memory _funders = new FunderInfo[](numberOfFunders);
     for (uint index = 0; index < numberOfFunders; index++) {
-      _funders[index] = funders[index];
+      _funders[index] = funders[lookUpFunders[index]];
     }
 
     return _funders;
@@ -75,8 +94,11 @@ contract Faucet {
      const instance = await Faucet.deployed();
 
      1. add funds 
-     => await instance.addFunds({ from: accounts[0], value: 1500000000000000000 }) // 1.5 ETH
-     => await instance.addFunds({ from: accounts[1], value: 1700000000000000000 }) // 1.7 ETH
+     // 1.5 ETH
+     => await instance.addFunds({ from: accounts[0], value: 1500000000000000000 });
+     
+     // 1.7 ETH
+     => await instance.addFunds({ from: accounts[1], value: 1700000000000000000 });
 
      2. get fund
      => (await instance.getAvailableFund()).toString()
