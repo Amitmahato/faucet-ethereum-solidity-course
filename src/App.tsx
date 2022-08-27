6dffcfb36827a62aeab2516d243da5afba8546c1
Amit Mahato <amit643.mahato@gmail.com>
2022-08-28 00:34:14 +0545

    15.1 add view for current balance & buttons to donate & withdraw with bulma & custom css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>{124}</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn mr-2">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
